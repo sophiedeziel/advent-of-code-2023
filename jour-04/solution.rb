16e4afbdd87f8bc882455799f24258ae23bf232d
Sophie Déziel <courrier@sophiedeziel.com>
2024-12-04 22:01:42 -0500

    jour 4 partie 2

--- jour-04/solution.rb
+++ jour-04/solution.rb
@@ -5,16 +5,23 @@ class Solution < BaseSolution
     @input = File.open("jour-04/#{input_mode}.txt", 'r').map do |line|
       line.chomp.gsub(/Card\s+\d+:/, "").split("|").map { |numbers| numbers.split(" ").map(&:to_i) }
     end
+
+    @wins = @input.map do |(winning, mine)|
+      matches = winning.count { |number| mine.include?(number) }
+    end
   end
 
   def part1
-    @input.map do |(winning, mine)|
-      matches = winning.count { |number| mine.include?(number) }
-      1 << (matches-1)
-    end.sum
+    @wins.map { |n| 1 << (n-1) }.sum
   end
 
   def part2
-    @part2
+    @wins.each_with_index.each_with_object({}) do |(wins, index), counts|
+      counts[index + 1] ||= 1
+      wins.times do |i|
+        counts[index + i + 2] ||= 1
+        counts[index + i + 2] += counts[index + 1]
+      end
+    end.values.sum
   end
 end
